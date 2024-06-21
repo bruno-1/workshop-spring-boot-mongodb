f8c965b3d2db95e87d056dfd5f80475e1af90254
Bruno Serafini <bruno.serafini@pm.me>
2024-06-20 21:55:30 -0300

    Obtendo um post por id

---
+++ src/main/java/com/brunoserafini/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.brunoserafini.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.brunoserafini.workshopmongo.Repository.PostRepository;
+import com.brunoserafini.workshopmongo.domain.Post;
+import com.brunoserafini.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
